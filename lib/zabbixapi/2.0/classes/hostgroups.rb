c8981646b1c9fe71e7caa744602633c8527a2fb5
Arnaud Mesureur <arnaud.mesureur@gmail.com>
2013-04-19 19:11:12 -0700

    Removed definitely HostGroups#delete method (using Basic#delete instead)

--- lib/zabbixapi/2.0/classes/hostgroups.rb
+++ lib/zabbixapi/2.0/classes/hostgroups.rb
@@ -16,11 +16,5 @@ class ZabbixApi
     def key
       "groupid"
     end
-
-    # def delete(data)
-    #   result = @client.api_request(:method => "hostgroup.delete", :params => [data])
-    #   result.empty? ? nil : result['groupids'][0].to_i
-    # end
-
   end
 end
