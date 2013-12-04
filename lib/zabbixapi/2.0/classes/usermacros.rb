9c125210e463114ad6104fa74dd325804b5b66ee
Pavel Tsaregorodtsev <ixpict@gmail.com>
2013-12-04 15:09:54 +0400

    Add user macros support

--- lib/zabbixapi/2.0/classes/usermacros.rb
+++ lib/zabbixapi/2.0/classes/usermacros.rb
@@ -1,5 +1,9 @@
 class ZabbixApi
-  class UserMacros < Basic
+  class Usermacros < Basic
+    def array_flag
+      true
+    end
+
     def indentify
       "macro"
     end
@@ -8,23 +12,35 @@ class ZabbixApi
       "usermacro"
     end
 
-    def create
+    def create(data)
+      request(data, "usermacro.create", "hostmacroids")
     end
 
-    def create_global
+    def create_global(data)
+      request(data, "usermacro.createglobal", "globalmacroids")
     end
 
-    def delete
+    def delete(data)
+      request(data, "usermacro.delete", "hostmacroids")
     end
 
-    def delete_global
+    def delete_global(data)
+      request(data, "usermacro.deleteglobal", "globalmacroids")
     end
 
     def update
+      request(data, "usermacro.update", "hostmacroids")
     end
 
     def update_global
+      request(data, "usermacro.updateglobal", "globalmacroids")
     end
+
+    private
+      def request(data, method, result_key)
+        result = @client.api_request(:method => method, :params => data)
+        result.empty? ? nil : result[result_key][0].to_i
+      end
 
   end
 end
