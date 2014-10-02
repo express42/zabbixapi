000d337ec49ca30e7a6cf861f9450be8da05e645
knakayama <knakayama.sh@gmail.com>
2014-10-02 22:22:27 +0900

    add configuration method

---
+++ lib/zabbixapi/classes/configurations.rb
@@ -0,0 +1,37 @@
+class ZabbixApi
+  class Configurations < Basic
+
+    def array_flag
+      true
+    end
+
+    def method_name
+      "configuration"
+    end
+
+    def indentify
+      "host"
+    end
+
+    # Export configuration data as a serialized string
+    # * *Args*    :
+    # see available parameters: https://www.zabbix.com/documentation/2.2/manual/api/reference/configuration/export
+    # * *Returns* :
+    #   - String
+    def export(data)
+      @client.api_request(:method => "configuration.export", :params => data)
+    end
+
+    # Import configurations data from a serialized string
+    # * *Args*    :
+    # see available parameters: https://www.zabbix.com/documentation/2.2/manual/api/reference/configuration/import
+    # * *Returns* :
+    #   - True or False
+    def import(data)
+      result = @client.api_request(:method => "configuration.import", :params => data)
+      result
+    end
+
+  end
+end
+
