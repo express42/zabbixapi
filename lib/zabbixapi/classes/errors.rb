936309f9871ec7af7540dc6314198061c478851c
mr.The <me@mrthe.name>
2015-09-08 15:56:05 +0300

    Add error hash and error_message to exception

--- lib/zabbixapi/classes/errors.rb
+++ lib/zabbixapi/classes/errors.rb
@@ -1,11 +1,20 @@
 class ZabbixApi
 
   class BaseError < RuntimeError
-    attr_accessor :response
+    attr_accessor :response, :error, :error_message
 
     def initialize(message, response = nil)
       super(message)
       @response = response
+
+      set_error! if @response
+    end
+
+    private
+
+    def set_error!
+      @error         = @response["error"] rescue nil
+      @error_message = "#{@error['message']}: #{@error['data']}" rescue nil
     end
   end
 
