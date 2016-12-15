aa1e2aae5087c8ea50300f56b1c5db4de1548843
Jon B <jrbeilke@gmail.com>
2016-12-15 09:47:31 -0600

    remove applications overrides for get_full_data and get_id and use the inherited methods from basic instead

--- lib/zabbixapi/classes/applications.rb
+++ lib/zabbixapi/classes/applications.rb
@@ -11,21 +11,6 @@ class ZabbixApi
       "name"
     end
 
-    def get_full_data(data)
-      filter_params = {}
-      request_data = data.dup # Duplicate data, as we modify it. Otherwise methods that use data after calling get_full_data (such as get_id) will fail.
-
-      request_data.each { |key, value| filter_params[key] = request_data.delete(key) unless API_PARAMETERS.include?(key) }
-      @client.api_request(:method => "application.get", :params => request_data.merge({:filter => filter_params, :output => "extend"}))
-    end
-
-    def get_id(data)
-      result = get_full_data(data)
-      applicationid = nil
-      result.each { |app| applicationid = app['applicationid'].to_i if app['name'] == data[:name] }
-      applicationid
-    end
-
     def create_or_update(data)
       applicationid = get_id(:name => data[:name], :hostid => data[:hostid])
       applicationid ? update(data.merge(:applicationid => applicationid)) : create(data)
