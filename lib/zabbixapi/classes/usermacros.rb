4d07d01c99636222b91f050275f0c17d66633273
Jon Beilke <jrbeilke@gmail.com>
2016-12-19 11:13:14 -0600

    add get_id and get_full_data overrides for usermacros as well as global macro methods for the same

--- lib/zabbixapi/classes/usermacros.rb
+++ lib/zabbixapi/classes/usermacros.rb
@@ -8,6 +8,46 @@ class ZabbixApi
       "usermacro"
     end
 
+    def get_id(data)
+      log "[DEBUG] Call get_id with parameters: #{data.inspect}"
+
+      # symbolize keys if the user used string keys instead of symbols
+      data = symbolize_keys(data) if data.key?(indentify)
+      # raise an error if indentify name was not supplied 
+      name = data[indentify.to_sym]
+      raise ApiError.new("#{indentify} not supplied in call to get_id") if name == nil
+
+      result = request(data, "usermacro.get", "hostmacroid")
+
+      result.length > 0 && result[0].key?("hostmacroid") ? result[0]["hostmacroid"].to_i : nil
+    end
+
+    def get_id_global(data)
+      log "[DEBUG] Call get_id_global with parameters: #{data.inspect}"
+
+      # symbolize keys if the user used string keys instead of symbols
+      data = symbolize_keys(data) if data.key?(indentify)
+      # raise an error if indentify name was not supplied 
+      name = data[indentify.to_sym]
+      raise ApiError.new("#{indentify} not supplied in call to get_id") if name == nil
+
+      result = request(data, "usermacro.get", "globalmacroid")
+
+      result.length > 0 && result[0].key?("globalmacroid") ? result[0]["globalmacroid"].to_i : nil
+    end
+
+    def get_full_data(data)
+      log "[DEBUG] Call get_full_data with parameters: #{data.inspect}"
+
+      request(data, "usermacro.get", "hostmacroid")
+    end
+
+    def get_full_data_global(data)
+      log "[DEBUG] Call get_full_data_global with parameters: #{data.inspect}"
+
+      request(data, "usermacro.get", "globalmacroid")
+    end
+
     def create(data)
       request(data, "usermacro.create", "hostmacroids")
     end
@@ -17,11 +57,13 @@ class ZabbixApi
     end
 
     def delete(data)
-      request(data, "usermacro.delete", "hostmacroids")
+      data_delete = [data]
+      request(data_delete, "usermacro.delete", "hostmacroids")
     end
 
     def delete_global(data)
-      request(data, "usermacro.deleteglobal", "globalmacroids")
+      data_delete = [data]
+      request(data_delete, "usermacro.deleteglobal", "globalmacroids")
     end
 
     def update(data)
@@ -35,28 +77,44 @@ class ZabbixApi
     def get_or_create(data)
       log "[DEBUG] Call get_or_create with parameters: #{data.inspect}"
 
-      unless (id = request({:macro => data[:macro], :hostid => data[:hostid]}, "usermacro.get", "hostmacroid"))
+      unless (id = get_id(:macro => data[:macro], :hostid => data[:hostid]))
         id = create(data)
       end
       id
     end
 
+    def get_or_create_global(data)
+      log "[DEBUG] Call get_or_create_global with parameters: #{data.inspect}"
+
+      unless (id = get_id_global(:macro => data[:macro], :hostid => data[:hostid]))
+        id = create_global(data)
+      end
+      id
+    end
+
     def create_or_update(data)
-      hostmacroid = request({:macro => data[:macro], :hostid => data[:hostid]}, "usermacro.get", "hostmacroid")
+      hostmacroid = get_id(:macro => data[:macro], :hostid => data[:hostid])
       hostmacroid ? update(data.merge(:hostmacroid => hostmacroid)) : create(data)
+    end
+
+    def create_or_update_global(data)
+      hostmacroid = get_id_global(:macro => data[:macro], :hostid => data[:hostid])
+      hostmacroid ? update_global(data.merge(:globalmacroid => globalmacroid)) : create_global(data)
     end
 
     private
       def request(data, method, result_key)
-        result = @client.api_request(:method => method, :params => data)
-
-        # Zabbix has different result formats for gets vs updates on macros - boo!
-        if result.kind_of?(Hash) && result.key?(result_key)
-          result[result_key][0].to_i
-        elsif result.kind_of?(Array) && result.length > 0
-          result[0][result_key].to_i
+        # Zabbix has different result formats for gets vs updates
+        if method.include?(".get")
+          if result_key.include?("global")
+            result = @client.api_request(:method => method, :params => { :globalmacro => true, :filter => data })
+          else
+            result = @client.api_request(:method => method, :params => { :filter => data })
+          end
         else
-          nil
+          result = @client.api_request(:method => method, :params => data)
+
+          result.key?(result_key) && result[result_key].length > 0 ? result[result_key][0].to_i : nil
         end
       end
 
