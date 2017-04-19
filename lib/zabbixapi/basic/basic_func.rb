cf447d19eacf96c8dc254996d3e621e6a8da6ece
Jon Beilke <jrbeilke@gmail.com>
2017-04-19 15:34:38 -0500

    more copypasta with symbolize_keys

--- lib/zabbixapi/basic/basic_func.rb
+++ lib/zabbixapi/basic/basic_func.rb
@@ -27,11 +27,11 @@ class ZabbixApi
     def symbolize_keys(object)
       if object.is_a?(Array)
         object.each_with_index do |val, index|
-          object[index] = symbolize_keys!(val)
+          object[index] = symbolize_keys(val)
         end
       elsif object.is_a?(Hash)
         object.keys.each do |key|
-          object[key.to_sym] = symbolize_keys!(object.delete(key))
+          object[key.to_sym] = symbolize_keys(object.delete(key))
         end
       end
       object
