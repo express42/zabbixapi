7febd76915d7463685809ab6da39f3ada45577a0
Tero Auvinen <tero@seravo.fi>
2018-03-22 10:48:24 +0200

    Allow Zabbix API v3.4 access

--- lib/zabbixapi/client.rb
+++ lib/zabbixapi/client.rb
@@ -51,7 +51,7 @@ class ZabbixApi
         @proxy_port = @proxy_uri.port
         @proxy_user, @proxy_pass = @proxy_uri.userinfo.split(/:/) if @proxy_uri.userinfo
       end
-      unless api_version =~ /(2\.4|3\.[02])\.\d+/
+      unless api_version =~ /(2\.4|3\.[024])\.\d+/
         raise ApiError.new("Zabbix API version: #{api_version} is not support by this version of zabbixapi")
       end
       @auth_hash = auth
