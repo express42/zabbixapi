5e34b4547f19be8e46c8384f65e0a607f4601352
Kevin J. Smith <kevin.j.smith2@cerner.com>
2019-08-08 13:08:38 -0500

    Adding Events
    Signed-off-by: Kevin J. Smith <kevin.j.smith2@cerner.com>

---
+++ spec/event.rb
@@ -0,0 +1,87 @@
+require 'spec_helper'
+
+describe 'event' do
+  before do
+    @eventname = gen_name 'event'
+    @usergroupid = zbx.usergroups.create(name: gen_name('usergroup'))
+    @eventdata = {
+      name: @eventname,
+      eventsource: '0', # event source is a triggerid
+      status: '0', # event is enabled
+      esc_period: '120', # how long each step should take
+      def_shortdata: 'Email header',
+      def_longdata: 'Email content',
+      maintenance_mode: '1',
+      filter: {
+        evaltype: '1', # perform 'and' between the conditions
+        conditions: [
+          {
+            conditiontype: '3', # trigger name
+            operator: '2', # like
+            value: 'pattern' # the pattern
+          },
+          {
+            conditiontype: '4', # trigger severity
+            operator: '5', # >=
+            value: '3' # average
+          }
+        ]
+      },
+      operations: [
+        {
+          operationtype: '0', # send message
+          opmessage_grp: [ # who the message will be sent to
+            {
+              usrgrpid: @usergroupid
+            }
+          ],
+          opmessage: {
+            default_msg: '0', # use default message
+            mediatypeid: '1' # email id
+          }
+        }
+      ],
+      recovery_operations: [
+        {
+          operationtype: '11', # send recovery message
+          opmessage_grp: [ # who the message will be sent to
+            {
+              usrgrpid: @usergroupid
+            }
+          ],
+          opmessage: {
+            default_msg: '0', # use default message
+            mediatypeid: '1' # email id
+          }
+        }
+      ]
+    }
+  end
+
+  context 'when not exists' do
+    describe 'create' do
+      it 'should return integer id' do
+        eventid = zbx.events.create(@eventdata)
+        expect(eventid).to be_kind_of(Integer)
+      end
+    end
+  end
+
+  context 'when exists' do
+    before do
+      @eventid = zbx.events.create(@eventdata)
+    end
+
+    describe 'create_or_update' do
+      it 'should return id' do
+        expect(zbx.events.create_or_update(@eventdata)).to eq @eventid
+      end
+    end
+
+    describe 'delete' do
+      it 'should return id' do
+        expect(zbx.events.delete(@eventid)).to eq @eventid
+      end
+    end
+  end
+end
