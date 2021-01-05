ef5ae395ef6f643330dc49d172d484e7e3d25035
Anastas Dancha <anapsix@random.io>
2021-01-05 14:28:41 +0300

    remove debug statements
    Signed-off-by: Anastas Dancha <anapsix@random.io>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,26 +18,3 @@ def gen_name(prefix)
   suffix = rand(1_000_000_000)
   "#{prefix}_#{suffix}"
 end
-
-IRB ||= false
-
-if IRB
-  media_type_data = {
-    "name"=>"zz",
-    "type"=>2,
-    "server"=>nil,
-    "helo"=>nil,
-    "email"=>nil,
-    "path"=>nil,
-    "gsm_modem"=>"/dev/modemzzz",
-    "username"=>nil,
-    "password"=>nil
-  }.transform_keys(&:to_sym)
-
-  zbx.mediatypes.create_or_update(media_type_data)
-
-  zbx.mediatypes.dump_by_id(mediatypeid: 28)
-
-  zbx.mediatypes.get_id({name: "sms"})
-end
-
